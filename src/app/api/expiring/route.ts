1f9537cb9dbc7471559238f7cb3de18b267131fc
Mishalyn Mei I. <mishalyn@hawaii.edu>
2026-04-20 12:08:52 -1000

    Updated API Route (expiring items)

--- src/app/api/expiring/route.ts
+++ src/app/api/expiring/route.ts
@@ -1,26 +1,40 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
+/*
+  NOTE: Use this API Route to grab items that have or will expire within the week.
+  In the frontend, filter further to separate items that expire today, tomorrow, or keep it within the week.
+*/
+
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const ownerEmail = searchParams.get('owner');
   if (!ownerEmail) return NextResponse.json({ error: 'Owner email required' }, { status: 400 });
-  const now = new Date();
 
-  // TODO: Expiring within 1 day.
+  // Relevant Dates
+  const today = new Date();
 
-  // TODO: Expiring within 1 week.
+  const nextWeek = new Date();
+  nextWeek.setDate(today.getDate() + 7);
 
-  // TODO: Expired items.
-
-  // TODO: Remove old logic
-  const soon = new Date();
-  soon.setDate(now.getDate() + 1);
-
-  const expiringItems = await prisma.produce.findMany({
-    where: { owner: ownerEmail, expiration: { lte: soon } },
+  // Fetch Items
+  const expireItems = await prisma.produce.findMany({
+    where: {
+      owner: ownerEmail,
+      expiration: { not: null, lte: nextWeek },
+    },
+    orderBy: { expiration: 'asc' },
   });
-  // End old logic
 
-  return NextResponse.json({ expiringItems });
+  // Filter: < Today
+  const expiredItems = expireItems.filter(
+    (item) => item.expiration && item.expiration < today,
+  );
+
+  // Filter: Today -> Next Week
+  const expiringWithinWeek = expireItems.filter(
+    (item) => item.expiration && item.expiration >= today && item.expiration <= nextWeek,
+  );
+
+  return NextResponse.json({ expiredItems, expiringWithinWeek });
 }
