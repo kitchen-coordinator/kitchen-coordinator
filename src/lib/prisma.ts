cbbebeafa9f1ee2b3a5a6d1745f97f5d4bd6a385
KawikaKN <kknjr16@gmail.com>
2026-05-06 10:22:26 -1000

    f

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,9 +5,9 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 export const prisma = globalForPrisma.prisma
-?? new PrismaClient({
-  log: ['query'],
-});
+  ?? new PrismaClient({
+    log: process.env.NODE_ENV === 'production' ? ['error', 'warn'] : ['query', 'error', 'warn'],
+  });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
